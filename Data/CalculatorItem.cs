8a0a0705c0f9eb6351730c0ae2aa961755513d0f
J-Wales <j.wales@edu.sait.ca>
2024-02-24 14:44:21 -0700

    First draft of calculator code.

---
+++ Data/CalculatorItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Data
+{
+	public class CalculatorItem
+	{
+		public double numOne {  get; set; }
+		public double numTwo { get; set; }
+	}
+}
---
+++ Data/NonNumericInputException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Data
+{
+	internal class NonNumericInputException : Exception
+	{
+		public NonNumericInputException() : base("Please enter valid numbers.") { }
+	}
+}
